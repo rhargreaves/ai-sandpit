11d419ea22d133caf11cf07c676c2ed5514cbf6f
Robert Hargreaves <kiyouta@gmail.com>
2025-01-23 15:08:02 +0000

    Make it interactive and record fun answers

--- huggingface/sandpit/sentiment-analysis.py
+++ huggingface/sandpit/sentiment-analysis.py
@@ -1,5 +1,24 @@
 from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis")
-sentiment = classifier("I've been waiting for a HuggingFace course my whole life.")
-print(sentiment)
+while True:
+	text = input("Enter a text to perform sentiment analysis on: ")
+	sentiment = classifier(text)
+	print(sentiment)
+
+# Some interesting results:
+
+# Isn't it a nice day outside?
+# [{'label': 'NEGATIVE', 'score': 0.8359090089797974}]
+
+# What a nice day
+# [{'label': 'POSITIVE', 'score': 0.9998655319213867}]
+
+# Pigeons fly often
+# [{'label': 'POSITIVE', 'score': 0.9531066417694092}]
+
+# Pigeons fly often in my face
+# [{'label': 'NEGATIVE', 'score': 0.9815180897712708}
+
+# Pigeons fly often in my face displaying their rich plummage
+# [{'label': 'POSITIVE', 'score': 0.9767320156097412}]
