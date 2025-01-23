3e9b7dbf681b0f2015084377ad3a2f0230032d57
Robert Hargreaves <kiyouta@gmail.com>
2025-01-23 16:03:51 +0000

    Print all generated texts

--- huggingface/sandpit/text-generation.py
+++ huggingface/sandpit/text-generation.py
@@ -4,4 +4,5 @@ classifier = pipeline("text-generation", model="distilgpt2")
 while True:
     text = input("Enter initial text: ")
     generated_text = classifier(text, num_return_sequences=2, max_length=30)
-    print(generated_text[0].get("generated_text"))
+    for t in generated_text:
+        print(t.get("generated_text"))
